7bf899fd1b60cc2c05bd1fbd7e3d838e34349559
Vytautas <vytautas101@inbox.lt>
2019-07-14 18:45:59 +0300

    Added item and remove item functionality in checkout item

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const CartActionTypes = {
   TOOGLE_CART_HIDDEN: 'TOOGLE_CART_HIDDEN',
-  ADD_ITEM: 'ADD_ITEM'
+  ADD_ITEM: 'ADD_ITEM',
+  CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART',
+  REMOVE_ITEM: 'REMOVE_ITEM'
 }
 
 export default CartActionTypes
